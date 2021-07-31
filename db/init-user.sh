90f183ef6cc987001aec8eee9d6d13b8c993f251
henchiyb <ducnhanyb@gmail.com>
2021-07-31 13:00:42 +0900

    Add profiler

--- db/init-user.sh
+++ db/init-user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat <<'EOF' | mysql -uroot
+cat <<'EOF' | mysql -uroot -h '172.31.32.40'
 CREATE USER 'isucon'@'%' IDENTIFIED BY 'isucon';
 GRANT ALL ON torb.* TO 'isucon'@'%';
 CREATE USER 'isucon'@'localhost' IDENTIFIED BY 'isucon';
